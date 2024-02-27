5f4f8e9abbc1e0c37e960431b22e1ce8c722ab08
AbeGue02 <abrahamgue02@gmail.com>
2024-02-27 10:14:38 -0500

    Finishes lesson

---
+++ router-intro/src/components/ComponentA.tsx
@@ -0,0 +1,5 @@
+export default function ComponentA () {
+    return (
+        <h1>I am ComponentA</h1>
+    )
+}
---
+++ router-intro/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import Nav from "./Nav"
+
+export default function Header () {
+    return (
+        <div>
+            <h1>I am the Header</h1>
+            <Nav/>
+        </div>
+    )
+}
